cd14e2468371f75c3abff228fd61d470b507dada
AbyChan <crystal.tyan@gmail.com>
2015-08-11 23:58:55 +0800

    calls listener when watch value is first undefined; add socpe value init value

--- src/scope.js
+++ src/scope.js
@@ -4,10 +4,15 @@ function Scope() {
   this.$$watchers = [];
 }
 
+function initWatchVal() {}
+
 Scope.prototype.$watch = function(watchFn, listenerFn){
   var watcher = {
     watchFn: watchFn,
-    listenerFn: listenerFn
+    listenerFn: listenerFn,
+    last: initWatchVal //add init so digest awary have their listener functions invoked
+                       //ex. value is undefined
+                       //initWatchval just a value
   };
   this.$$watchers.push(watcher);
 };
@@ -16,7 +21,7 @@ Scope.prototype.$digest = function(){
   var self = this;
   var newValue, oldValue;
   _.forEach(this.$$watchers, function(watcher){
-    newValue = watcher.watchFn(self);
+    newValue = watcher.watchFn(self); //watchFn return new value
     oldValue = watcher.last;
     if( newValue !== oldValue ) {
       watcher.last = newValue;
