0cdda2dcc5e7e95e9f02ec116b9a44a137c97dd9
AbyChan <crystal.tyan@gmail.com>
2015-08-14 00:42:00 +0800

    compares based on value if enabled

--- src/scope.js
+++ src/scope.js
@@ -7,10 +7,11 @@ function Scope() {
 
 function initWatchVal() {}
 
-Scope.prototype.$watch = function(watchFn, listenerFn){
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq){
   var watcher = {
     watchFn: watchFn,
     listenerFn: listenerFn || function() {},//put an empty no-op function in its place
+    valueEq: !!valueEq,
     last: initWatchVal //add init so digest awary have their listener functions invoked
                        //ex. value is undefined
                        //initWatchval just a value
@@ -25,9 +26,9 @@ Scope.prototype.$$digestOnce = function(){
   _.forEach(this.$$watchers, function(watcher){
     newValue = watcher.watchFn(self); //watchFn return new value
     oldValue = watcher.last;
-    if( newValue !== oldValue ) {
+    if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
       self.$$lastDirtyWatch = watcher;
-      watcher.last = newValue;
+      watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
       watcher.listenerFn(newValue,
                          (oldValue === initWatchVal ? newValue : oldValue),
                          self); //when first changed shoudln't return initwatchval;
@@ -51,4 +52,10 @@ Scope.prototype.$digest = function(){
   } while (dirty);
 };
 
-
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq) {
+  if ( valueEq ) {
+    return _.isEqual(newValue, oldValue);
+  } else {
+    return newValue === oldValue;
+  }
+};
