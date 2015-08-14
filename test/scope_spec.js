8ea4bedfbd44201c913e414139dc0d2e088867bf
AbyChan <crystal.tyan@gmail.com>
2015-08-14 23:09:45 +0800

    $eval

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -215,7 +215,25 @@ describe('Scope', function() {
       expect(scope.counter).toBe(1);
     });
 
+    it('executes $eval\'ed function and returns result', function(){
+      scope.aValue = 42;
 
+      var result = scope.$eval(function(scope){
+        return scope.aValue;
+      });
+
+      expect(result).toBe(42);
+    });
+
+    it('it passes the second $eval argument straight throught',function(){
+      scope.aValue = 42;
+
+      var result = scope.$eval(function(scope, arg){
+        return scope.aValue + arg;
+      }, 2);
+
+      expect(result).toBe(44);
+    });
 
   });
 });
